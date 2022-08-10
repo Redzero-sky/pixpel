921a821c08124f962ee756ddfc76332beedefee5
Redzero-sky <markofrei94@gmail.com>
2022-08-11 01:43:46 +0900

    Initial commit

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import "./Modal.scss"
+
+const Modal = (props) => {
+	return (
+		<div className="modal">
+			<div className="modal-container">
+				<div className="modal-head">
+					{props.header}
+					<button className="btn btn--secondary close" onClick={props.handleClose}>&times;</button>
+				</div>
+				{props.children}
+			</div>
+		</div>
+	)
+}
+
+export default Modal
---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import AddressManagement from "./AddressManagement/AddressManagement";
+import Exchange from "./Exchange/Exchange";
+import HelpCenter from "./GameMarket/GameMarket";
+import Home from "./Home/Home";
+import Inventory from "./Inventory/Inventory";
+import NFTMarket from "./NFTMarket/NFTMarket";
+import Notification from "./Notification/Notification";
+import Profile from "./Profile/Profile";
+import Stacking from "./Stacking/Stacking";
+import SwapMaster from "./SwapMaster/SwapMaster";
+import UserHome from "./UserHome/UserHome";
+import Wallet from "./Wallet/Wallet";
+import Withdraw from "./Withdraw/Withdraw";
+import GameMarket from "./GameMarket/GameMarket";
+export {
+    AddressManagement,
+    Home,
+    NFTMarket,
+    Stacking,
+    Exchange,
+    HelpCenter,
+    Inventory,
+    Notification,
+    Profile,
+    SwapMaster,
+    UserHome,
+    Wallet,
+    Withdraw,
+    GameMarket
+}
